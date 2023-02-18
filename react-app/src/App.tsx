7a99fd1c20afb2e345e5924734821a94b93473a9
danjool <horrabin@gmail.com>
2023-02-18 13:21:51 -0700

    init react

---
+++ react-app/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+  const results = 'Init'
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          {results}
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
