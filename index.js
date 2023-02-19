c676e55dda9ee126c2b06a960604aa92a9940356
danjool <horrabin@gmail.com>
2023-02-18 17:11:08 -0700

    cleanup

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require('express'); 
+const express = require('express') 
 const path = require('path')
-const app = express(); 
+const app = express() 
 const SQL = require('sql-template-strings')
 const { Client } = require('pg')
 
-const port = process.env.PORT || 8000;
-const dbhost = process.env.DBHOST || '35.212.167.150';
-const dbuser = process.env.DBUSER || 'postgres';
-const dbpassword = process.env.DBPASSWORD || '';
-const dbport = process.env.DBPORT || 5432;
+const port = process.env.PORT || 8000
+const dbhost = process.env.DBHOST || '35.212.167.150'
+const dbuser = process.env.DBUSER || 'postgres'
+const dbpassword = process.env.DBPASSWORD || ''
+const dbport = process.env.DBPORT || 5432
 
 console.log("dbpassword", dbpassword)
 
@@ -19,19 +19,18 @@ const client = new Client({
     port: dbport
 })
 
-// This displays message that the server running and listening to specified port
-app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
 
 // create a GET route
-app.get('/express_backend', (req, res) => { //Line 9
-    console.log("express backend")
+app.get('/express_backend', (req, res) => {
     client.connect()
-    client.query(SQL`Select * FROM users;`, (err, dbResponse)=>{
+    client.query(SQL`Select * FROM users`, (err, dbResponse)=>{
         if(err) throw err
         client.end()
-        res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT', dbResponse: dbResponse.rows }); //Line 10
+        res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT', dbResponse: dbResponse.rows })
     })
     
-});
+})
 
-app.use(express.static(path.join(__dirname, 'react-app', 'build')));
+app.use(express.static(path.join(__dirname, 'react-app', 'build')))
